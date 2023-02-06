20659260a831fd006e3c2fcab3990ca79d277921
SATYARTH <satyarthtiwari123@gmail.com>
2023-02-06 15:39:30 +0530

    LEETCODE 2068

---
+++ stringEquivalent.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        HashMap<Character,Integer> hm = new HashMap();
+        for (int i = 0; i < word1.length(); i++) {
+            hm.put(word1.charAt(i), hm.getOrDefault(word1.charAt(i), 0) + 1);
+            hm.put(word2.charAt(i), hm.getOrDefault(word2.charAt(i), 0) - 1);
+        }
+        for (int i : hm.values()) if (i > 3 || i < -3)  return false;
+        return true;
+    }
+}
