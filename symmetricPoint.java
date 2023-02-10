3d12eb783de1cace255ef475b165bb0d214268ac
SATYARTH <satyarthtiwari123@gmail.com>
2023-02-10 18:07:40 +0530

    gfg problem

--- symmetricPoint.java
+++ symmetricPoint.java
@@ -14,13 +14,15 @@ import java.util.HashMap;
 public class symmetricPoint {
 
 	public static void main(String[] args) {
+		
 		int arr[][]= {{11,20},{30,40},{5,10},{40,30},{10,5}};
-		HashMap<Integer,Integer> hm=new HashMap<>();
+		
+		HashMap<Integer,Integer> hm=new HashMap<>();  // hashmap to store the key value pair
 		
 		for(int[] e:arr) {
 
-			if(hm.containsKey(e[1]) && hm.containsValue(e[0]))
-				System.out.println(e[1]+" "+hm.get(e[1]));
+			// this will check if the value is present as key already or not!
+			if(hm.containsKey(e[1]) && hm.containsValue(e[0])) System.out.println(e[1]+" "+hm.get(e[1]));
 			
 			else hm.put(e[0],e[1]);
 		}
