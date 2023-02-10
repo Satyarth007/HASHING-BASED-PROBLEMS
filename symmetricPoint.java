c9b8b1624321bb977bee8e78323540ad7f515812
SATYARTH <satyarthtiwari123@gmail.com>
2023-02-10 18:06:12 +0530

    gfg problem

--- symmetricPoint.java
+++ symmetricPoint.java
@@ -1,3 +1,15 @@
+/*Two pairs (a, b) and (c, d) are said to be symmetric if c is equal to b and a is equal to d.
+For example, (10, 20) and (20, 10) are symmetric. Given an array of pairs find all symmetric pairs in it. 
+It may be assumed that the first elements of all pairs are distinct.
+Example: 
+
+Input: a[] = {{11, 20}, {30, 40}, {5, 10}, {40, 30}, {10, 5}}
+Output: Following pairs have symmetric pairs
+        (30, 40)
+        (5, 10)
+        
+*/
+// my code is more easy to understand:-
 import java.util.HashMap;
 public class symmetricPoint {
 
@@ -6,8 +18,7 @@ public class symmetricPoint {
 		HashMap<Integer,Integer> hm=new HashMap<>();
 		
 		for(int[] e:arr) {
-		//	if(hm.isEmpty()) hm.put(e[0],e[1]);
-			
+
 			if(hm.containsKey(e[1]) && hm.containsValue(e[0]))
 				System.out.println(e[1]+" "+hm.get(e[1]));
 			
